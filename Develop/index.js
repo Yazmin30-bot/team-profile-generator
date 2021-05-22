0c422a6bc874a349001c3a2fa8abcd2dfbff967e
Yazmin Tinoco <ytip30@gmail.com>
2021-05-22 00:19:30 -0500

    Prompt manager questions

---
+++ Develop/index.js
@@ -0,0 +1,64 @@
+//Import dependencies and classes
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const fs = require('fs');
+const replace = require('replace');
+const path = require('path');
+
+const employees = [];
+const memberInfoFinal = [];
+
+//Prompt managerQuestions
+const managerQuestion = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?",
+        validate: answer => {
+            if (answer !== '') {
+                return true;
+            }
+            return "Please enter at least one character."
+        }
+
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the team manager's id?",
+        validate: answer => {
+            if (/^[0-9]+$/.test(answer)) {
+                return true;
+            }
+            return "Please enter at least one character"
+        }
+
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the team manager's email?",
+        validate: answer => {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(answer)) {
+                return true;
+            }
+            return "Please enter a valid email address."
+        }
+
+    },
+    {
+        type: "input",
+        name: "phone",
+        message: "What is the team manager's office number?",
+        validate: answer => {
+            if (/^[0-9]+$/.test(answer)) {
+                return true;
+            }
+            return "Please enter at least one character"
+        }
+    },
+
+
+];
